236a7c6a7de6b46fe39d4ed3613c489a719e8f7b
JueLing <hcj1677041538@gmail.com>
2021-08-13 10:30:56 +0800

    Update bin.sh

--- bin.sh
+++ bin.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # 修改默认IP
-sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+#sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 # 修改默认主题
 #sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 #修改主机名
 #sed -i 's/OpenWrt/Bin-Lean/g' package/base-files/files/bin/config_generate
 #关闭自建私有源签名验证
-sed -i '90d' package/system/opkg/Makefile
+#sed -i '90d' package/system/opkg/Makefile
 #开启不可描述功能
 echo "src-git helloworld https://github.com/fw876/helloworld" >> feeds.conf.default
 #关闭无用功能
@@ -14,7 +14,7 @@ sed -i 's/src-git telephony/#src-git telephony/g' feeds.conf.default
 
 #添加额外软件包
 #git clone https://github.com/xiaorouji/openwrt-passwall.git package/openwrt-packages/luci-app-passwall
-svn co https://github.com/siropboy/mypackages/trunk/luci-app-control-timewol package/openwrt-packages/luci-app-control-timewol
+#svn co https://github.com/siropboy/mypackages/trunk/luci-app-control-timewol package/openwrt-packages/luci-app-control-timewol
 git clone https://github.com/tty228/luci-app-serverchan.git package/openwrt-packages/luci-app-serverchan
 git clone https://github.com/binge8/luci-theme-argon-mc.git package/openwrt-packages/luci-theme-argon-mc
 git clone https://github.com/Leo-Jo-My/luci-theme-opentomcat.git package/openwrt-packages/luci-theme-opentomcat
